b0d707295aefff2441ad5afe8be16ce6a9be1e1f
Salvador Poeta <dsmlhidroponia@gmail.com>
2026-03-22 04:56:33 -0600

    add text-blue-400 to animate spin in buttons

--- src/components/DetailScraper.jsx
+++ src/components/DetailScraper.jsx
@@ -170,7 +170,7 @@ const DetailScraper = () => {
           {loading ? (
             <>
               <svg
-                className="animate-spin h-5 w-5 text-gray-400"
+                className="animate-spin h-5 w-5 text-blue-400"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
