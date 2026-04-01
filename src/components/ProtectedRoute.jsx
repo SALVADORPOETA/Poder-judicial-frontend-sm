01cefb3b1bc0b0d033e1bbf992ff497748cad635
Salvador Poeta <dsmlhidroponia@gmail.com>
2026-04-01 05:56:51 -0600

    fix ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,8 +1,14 @@
 import { Navigate } from 'react-router-dom'
 
 const ProtectedRoute = ({ children }) => {
+  // No dependas solo del "state" de App, lee el storage directamente aquí
   const isAuth = sessionStorage.getItem('isAuth') === 'true'
-  return isAuth ? children : <Navigate to="/" replace />
+
+  if (!isAuth) {
+    return <Navigate to="/" replace />
+  }
+
+  return children
 }
 
 export default ProtectedRoute
